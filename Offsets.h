f05f30b422eadc0e0bc3df99925e7e9bcc0fac82
Joe <93642070+Dr-R0BOT@users.noreply.github.com>
2021-12-07 13:37:44 -0800

    Xd

---
+++ Offsets.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Includes.h"
+
+namespace Offset
+{
+	const DWORD GWorld = 0xA049BA8;
+	const DWORD FNamePool = 0x9E36BC0 + 0x24B8;
+	const DWORD GUObject = 0x9EE3ED8;
+	const DWORD CanvasMap = 0xA01AF78;
+	const DWORD PersistentLevel = 0x20;
+	const DWORD PlayerController = 0x108;
+	const DWORD RootComponent = 0x2b8;
+	const DWORD CurrentStates = 0x118;
+	const DWORD Name = 0x3d0;
+	const DWORD Mesh = 0x68;
+	const DWORD BodyAddv = 0x150;
+	const DWORD MinLOD = 0xd0;
+	const DWORD PlayerCameraManager = 0x5c8;
+	const DWORD RelativeLocation = 0x24c;
+	const DWORD POV = 0x10;
+	const DWORD Location = 0x18;
+	const DWORD Rotation = 0x18;
+	const DWORD FOV = 0x904;
+	const DWORD Velocity = 0x110;
+	const DWORD JumpForce = 0x3d8;
+	const DWORD Health = 0x1a00;
+	const DWORD HealthMax = 0x35b;
+
+}
